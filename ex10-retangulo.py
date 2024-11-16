14a1c7afa879e1a330a2ccea4ab4662a03758c34
Fernando <ironitabarbosa@gmail.com>
2024-11-16 19:42:20 -0400

    ex10 finalizado

---
+++ ex10-retangulo.py
@@ -0,0 +1,19 @@
+# Preciso criar um programa que calcule a área de um retangulo e exiba o resultado, utilizando a seguinte fórmula: area = base ∗ altura.
+
+# (Calculadora de Área de Retângulo) Receba a base e a altura de um retângulo e exiba a área dele usando a fórmula: area = base ∗ altura 2 (2) area = base × altura.
+
+def retangulo():
+    print("Bem-vindo à calculadora de área de retângulo!")
+    
+    x = True
+    while x:
+        base = float(input("Digite a base do retângulo: \n"))
+        altura = float(input("Digite a altura do retângulo: \n"))
+        area =  base * altura
+
+        print(f"A área do retângulo é: {area}m²")
+        
+        sair = input("Deseja sair? (s/n): \n").lower()
+        if sair == "s":
+            x = False
+retangulo()
