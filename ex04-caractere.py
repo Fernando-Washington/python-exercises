b82b056c97cbc59787a7118d514589a0fba1400b
Fernando <ifernandowashington@gmail.com>
2024-11-15 21:33:31 -0400

    ex04 finalizado

--- ex04-caractere.py
+++ ex04-caractere.py
@@ -1,13 +1,16 @@
 # preciso fazer um contador de caracteres aonde conte os caracteres de uma palavra levando em consideração espaços em branco. para isso podemos utilizar a propriedade len()
 
 # (Contador de Caracteres em uma Palavra) Solicite uma palavra do usuário e exiba quantos caracteres ela possui.
+def contador():
+    print("Bem vindo ao contador de caracteres!")
 
-print("Bem vindo ao contador de caracteres!")
-
-while True:
-    caractere = input("Digite seu texto para descobrir o número de caracteres: \n")
-    print(f"O texto possui {len(caractere)} caracteres")
-    sair = input("Deseja sair? (s/n): \n").lower()
-    if sair == "s":
-        print("Obrigado por usar o contador de caracteres!")
-        break
+    x = True
+    while x:
+        caractere = input("Digite seu texto para descobrir o número de caracteres: \n")
+        print(f"O texto possui {len(caractere)} caracteres")
+        sair = input("Deseja sair? (s/n): \n").lower()
+        if sair == "s":
+            print("Obrigado por usar o contador de caracteres!")
+            x = False
+contador()
+            
