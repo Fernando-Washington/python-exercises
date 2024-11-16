a5eb17eea08acf0c4fa8ac1811ad1065869493b4
Fernando <ifernandowashington@gmail.com>
2024-11-15 21:40:26 -0400

    ex05 finalizado

--- ex05-tabuada.py
+++ ex05-tabuada.py
@@ -1,16 +1,19 @@
 # preciso fazer um programa que receba um numero inteiro e gere e exiba a tabuada dele de 1 a 10. para isso podemos utilizar um laço de repetição. acho melhor o for pois podemos limitar o laço de repetição.
 
 # (Gerador de Tabuada) Receba um número inteiro e exiba a tabuada dele (multiplicações de 1 a 10).
-print("Bem vindo ao super gerador de tabuada 2.0!")
-
-while True:
-    num = int(input("Digite um número inteiro: \n"))
-    print(f"Tabuada do {num}:")
+def tabuada():
+    print("Bem vindo ao super gerador de tabuada 2.0!")
     
-    for i in range(1, 10 + 1):
-        print(f"{num} x {i} = {num * i}")
+    x = True
+    while x:
+        num = int(input("Digite um número inteiro: \n"))
+        print(f"Tabuada do {num}:")
         
-    sair = input("Deseja sair? (s/n): \n").lower()
-    if sair == "s":
-        print("Obrigado por usar o super gerador de tabuada 2.0!")
-        break
+        for i in range(1, 10 + 1):
+            print(f"{num} x {i} = {num * i}")  
+                      
+        sair = input("Deseja sair? (s/n): \n").lower()
+        if sair == "s":
+            print("Obrigado por usar o super gerador de tabuada 2.0!")
+            x = False
+tabuada()
