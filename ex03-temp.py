3b81f4c5f18b8436d0ef5e4cccd65e80bf79819e
Fernando <ifernandowashington@gmail.com>
2024-11-15 21:28:27 -0400

    ex03 finalizado

--- ex03-temp.py
+++ ex03-temp.py
@@ -1,18 +1,21 @@
 # preciso fazer um programa que converta uma temperatura de graus Celcius para Fahrenheit. Para isso, é só receber a temperatura em graus Celcius e utilizar a fórmula.
 
 # (Conversor de Temperatura) Receba uma temperatura em graus Celsius e convertapara Fahrenheit usando a fórmula: F = C ∗ 1.8 + 32.
-
-print("Bem-vindo ao conversor de temperatura!")
-print("Faça a conversão utilizando ponto ao invés de vírgula \n") 
-
-while True:
-    celcius = float(input("Digite a temperatura em graus Celcius: \n"))
-    r_farenheit = celcius * 1.8 + 32
-    print(f"A temperatura em graus Farenheit é: {r_farenheit}")
+def conversor_temperatura():
+    print("Bem-vindo ao conversor de temperatura!")
+    print("Faça a conversão utilizando ponto ao invés de vírgula \n") 
     
-    sair = input("Deseja sair? (s/n) \n").lower()
-    if sair == "s":
-        print("Obrigado por usar o conversor de temperatura!")
-        break
-    
+    x = True # round((nota_1 + nota_2 + nota_3) / 3, 2)
+    while x:
+        celcius = float(input("Digite a temperatura em graus Celcius: \n"))
+        r_farenheit = celcius * 1.8 + 32
+        print(f"A temperatura em graus Farenheit é: {r_farenheit}")
+        sair = input("Deseja sair? (s/n) \n").lower()
+        
+        if sair == "s":
+            print("Obrigado por usar o conversor de temperatura!")
+            x = False
+            
+conversor_temperatura()
+
 # não consegui limitar a quantidade de casas decimais :[ 
